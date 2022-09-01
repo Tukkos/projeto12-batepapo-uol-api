09258475ce534c8ef598292dc1451cfddc831905
Tukkos <jpfcastro@id.uff.br>
2022-09-01 11:47:19 -0300

    feat: dotenv, mongodb and dayjs installed; post to /participants; get to /participants; post to /messages iniciated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import dayjs from "dayjs";
 
+let now = dayjs();
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
-//teste teste
+const mongoCLient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoCLient.connect().then(() => {
+    db = mongoCLient.db("batePapoUol");
+});
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.sendStatus(422);
+    }
+
+    //QUANDO TIVER TEMPO PEDIR AJUDA PRO THIAGO PRA TRANSFORMAR PRA FILTER--------------------------------------
+    const participants = await db.collection("participants").find().toArray();
+    for (let i = 0; i < participants.length; i++) {
+        if (name === participants[i].name) {
+            return res.status(409).send("Usuário já existente");
+        }
+    };
+    await db.collection("participants").insertOne({
+        name,
+        lastStatus: Date.now()
+    });
+    res.sendStatus(201);
+});
+
+app.get("/participants", async (req, res) => {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+});
+
+app.post("/messages", async (req, res) => {
+    const time = now.format("HH:mm:ss");
+    const user = req.headers.user;
+    const { to, text, type } = req.body;
+
+    // if (!to || !text) {
+    //     return res.sendStatus(422);
+    // }
+    // if (type !== "message" && type !== "private_message") {
+    //     return res.sendStatus(422);
+    // }
+    await db.collection("messages").insertOne({
+        from: user,
+        to,
+        text,
+        type,
+        time: time
+    });
+    res.sendStatus(201);
+})
+
 
 app.listen(5000, () => console.log("Listen on http://localhost:5000"));
