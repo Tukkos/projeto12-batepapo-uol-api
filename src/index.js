6dcf749d94f7267a7c079648088d19d4cc054416
Tukkos <jpfcastro@id.uff.br>
2022-09-02 13:27:50 -0300

    feat: post /messages validation with joi; user validation created

--- src/index.js
+++ src/index.js
@@ -22,11 +22,9 @@ const participantsSchema = joi.object({
 });
 
 const messagesSchema = joi.object({
-    from: joi.string().required(),
     to: joi.string().required(),
     text: joi.string().required(),
-    type: joi.string().required(),
-    time: joi.string().required()
+    type: joi.string().required().valid("message", "private_message")
 });
 
 app.post("/participants", async (req, res) => {
@@ -37,18 +35,17 @@ app.post("/participants", async (req, res) => {
         return res.status(422).send(validation.error.details[0].message);
     }
 
-    //QUANDO TIVER TEMPO PEDIR AJUDA PRO THIAGO PRA TRANSFORMAR PRA FILTER--------------------------------------
+    //Forma verbosa------------------------------------------------------------------
     // const participants = await db.collection("participants").find().toArray();
     // for (let i = 0; i < participants.length; i++) {
     //     if (name === participants[i].name) {
     //         return res.status(409).send("Usuário já existente");
     //     }
     // };
-    
-
+    //Forma refatorada---------------------------------------------------------------
     const participants = await db.collection("participants").find().toArray();
     if (participants.find(participant => participant.name === name)) {
-        return (res.status(409).send("Usuário já existente"));
+        return res.status(409).send("Usuário já existente");
     };
 
     try {
@@ -59,13 +56,16 @@ app.post("/participants", async (req, res) => {
         res.sendStatus(201);
     } catch (error) {
         res.send(500).send(error.message);
-    }
-    
+    };
 });
 
 app.get("/participants", async (req, res) => {
-    const participants = await db.collection("participants").find().toArray();
-    res.send(participants);
+    try {
+        const participants = await db.collection("participants").find().toArray();
+        res.send(participants);
+    } catch (error) {
+        res.send(500).send(error.message);
+    };
 });
 
 app.post("/messages", async (req, res) => {
@@ -73,20 +73,31 @@ app.post("/messages", async (req, res) => {
     const user = req.headers.user;
     const { to, text, type } = req.body;
 
-    if (!to || !text) {
-        return res.sendStatus(422);
+    const validation = messagesSchema.validate(req.body, {abortEarly: false});
+    if (validation.error) {
+        const error = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(error);
+    };
+
+    const participants = await db.collection("messages").find().toArray();
+    console.log(participants.map(participant => participant.from));
+    if (!participants.map(participant => participant.from).includes(user)) {
+        return res.status(422).send("Erro de usuário");
+    };
+
+    try {
+        await db.collection("messages").insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: time
+        });
+        res.sendStatus(201);
+    } catch (error) {
+        res.send(500).send(error.message);
     }
-    if (type !== "message" && type !== "private_message") {
-        return res.sendStatus(422);
-    }
-    await db.collection("messages").insertOne({
-        from: user,
-        to,
-        text,
-        type,
-        time: time
-    });
-    res.sendStatus(201);
+    
 });
 
 app.get("/messages", async (req, res) => {
